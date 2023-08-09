80b3cf5a53aef469865123a516c19b68158b28af
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-08-09 23:37:10 +0400

    add math contract for simplify

--- StrikerDays.sol
+++ StrikerDays.sol
@@ -81,4 +81,12 @@ contract StrikerDaysContract{
         return address(this).balance;
     }
 }
+contract Math {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x);
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x);
+    }
+}
 
